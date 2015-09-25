d9e92b8aaace17655ad1b93523fa036d7e96c2e6
Hunter John Larco <hjlarco@gmail.com>
2015-09-25 11:03:25 -0700

    Histogram (class model)

--- Histogram.py
+++ Histogram.py
@@ -2,32 +2,36 @@ import sys
 import random
 
 
-def histogram(text):
-  import re
-  stripped = re.sub('[^a-zA-Z\s]', '', text)
-  words = stripped.split(' ')
-  return histogram_from_array(words)
+class Histogram:
+  
+  # O(n) if given array
+  # O(2n) if given text
+  def __init__(self, arg):
+    self.__FormFromArray__(arg) if isinstance(arg, list) else self.__FormFromText__(arg)
+  
+  # O(n)
+  def __FormFromArray__(self, words):
+    histogram = {}
+    for word in words:
+      histogram[word] = 1 if not word in histogram else histogram[word]+1
+    self.structure = histogram
+    self.words     = words
+  
+  # O(1)
+  def getUniqueWords(self):
+    return self.structure.keys()
+  
+  # O(1)
+  def getFrequency(self, word):
+    return self.structure[word] if word in self.structure else 0
 
-def histogram_from_array(words):
-  histogram = {'meta': {'words': words}, 'histogram': {}}
-  for word in words:
-    histogram['histogram'][word] = 1 if not word in histogram['histogram'] else histogram['histogram'][word]+1
-  return histogram
-
-
-def unique_words(histogram):
-  return histogram['histogram'].keys()
-
-
-def frequency(word, histogram):
-  return histogram['histogram'][word]
 
 
 if __name__ == '__main__':
-  hist = histogram('hunter is the coolest hunter')
-  words = unique_words(hist)
-  freq = frequency('hunter', hist)
+  text = open('/Users/hunterlarco/Documents/Github Repos/twitter-bot-python-HunterLarco/Day7/endersgame.txt', 'r').read()
+  hist = Histogram(text.split(' '))
   
   print(hist)
-  print(words)
-  print(freq)
+  print(hist.getFrequency('I'))
+  print(hist.getFrequency('Ender'))
+  print(hist.getFrequency('I\'ll'))
